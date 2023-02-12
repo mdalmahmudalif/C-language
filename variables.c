13df2eb1dea78b7b816609329944678cd54e1349
Md Al Mahmud Alif <121686759+mdalmahmudalif@users.noreply.github.com>
2023-02-13 01:17:48 +0600

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+ char CharaterName[] = "Alex";
+ int CharaterAge = 23;
+printf("There was a man Named %s\n", CharaterName);
+printf("He is %d Years old\n", CharaterAge);
+printf("He Really liked the name %s\n", CharaterName);
+printf("But he don't like to being %d\n",CharaterAge);
+return 0;
+
+
+}
