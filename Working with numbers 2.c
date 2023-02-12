4b227f4c63e8cdfb1bb4ce1d96e5d8d024d29133
Md Al Mahmud Alif <121686759+mdalmahmudalif@users.noreply.github.com>
2023-02-13 01:18:34 +0600

    Add files via upload

---
+++ Working with numbers 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+
+printf("%d\n", pow(3, 3) );
+printf("%d\n", sqrt(64) );
+printf("%d\n", ceil(9.35) );
+printf("%d\n", floor(33.690) );
+
+return 0;
+
+}
---
+++ Working with numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+printf("%f", 3.0 + 6 );
+
+int Thus = 3;
+printf("%d", Thus);
+
+return 0;
+
+}
