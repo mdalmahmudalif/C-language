a8b9fff0153058770c3a8e1d613ce82e3e51e5d8
Md Al Mahmud Alif <121686759+mdalmahmudalif@users.noreply.github.com>
2023-02-13 02:11:33 +0600

    Add files via upload

---
+++ For Loops.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+   /*   int J = 3;
+      while (J <= 9){
+       printf("%d\n", J );
+       J++;
+      }
+      */
+
+    /*
+      int R;
+      for (R = 1; R <= 6; R++){
+        printf("%d\n", R );
+      }
+      */
+
+      int TopicNum[] ={10, 20, 30, 40, 50, 60, 70};
+      TopicNum [2];
+      int R;
+      for (R = 1; R <= 6; R++){
+        printf("%d\n", TopicNum [R] );
+      }
+
+return 0;
+}
---
+++ While Loops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+
+    int index = 1;
+    while (index <= 9){
+    printf("%d\n", index);
+    index = 1 + index;
+   /*or index++; */
+    }
+
+    return 0;
+}
