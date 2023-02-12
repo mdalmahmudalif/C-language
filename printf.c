fef8ae54e1709d5b5281a4cdef6d2700e10321d3
Md Al Mahmud Alif <121686759+mdalmahmudalif@users.noreply.github.com>
2023-02-13 01:17:31 +0600

    Add files via upload

---
+++ printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+printf("You can write any text on this printf function that will print out on the Console Screen\n");
+printf("My Favorite Number is %d\n", 333);
+printf("My Favorite %s is %f\n", "point", 369.30);
+
+int FavNum = 12;
+char MyChar = 'B';
+printf("My Favorite %c is %f", MyChar, FavNum);
+
+return 0;
+
+}
