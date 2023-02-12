cb7ad9b88dbd77ebf426cfe1747199b4a9556483
Md Al Mahmud Alif <121686759+mdalmahmudalif@users.noreply.github.com>
2023-02-13 02:13:26 +0600

    Add files via upload

---
+++ Dereferencing Pointers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main (){
+ /*
+    Normal
+
+    int age = 33;
+    int *pAge = &age;
+
+    printf("%p", *pAge);
+
+    Blew Dereferencing Pointers Ex:
+*/
+
+    int age = 33;
+    int *pAge = &age;
+
+    printf("%d", *pAge);
+
+
+return 0;
+
+}
